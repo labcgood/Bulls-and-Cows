ccd49f193b8503464215ce85afd25c13fcd5b9e9
Rabbit <labcgood44@gmail.com>
2024-02-02 16:29:03 +0800

    Initial Commit

---
+++ Bulls and Cows/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bulls and Cows
+//
+//  Created by Labe on 2024/2/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
